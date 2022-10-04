a3472892cc396be5de90c8912f589abbc51eefb6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 05:56:30 +0000

    Adding numeric_owner as keyword arguement

--- pkgs/datasets/cars196.py
+++ pkgs/datasets/cars196.py
@@ -44,7 +44,7 @@ class CARS196Dataset(BaseSplitImageDataset):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=extract_folder)
